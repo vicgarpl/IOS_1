e830c84a696ef0963f939273a914caf90ee8d1d2
Victor Danney Garcia Plaza <vdgp@yahoo.com>
2015-12-06 19:51:54 -0500

    Initial Commit

---
+++ TEST_IOS_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TEST_IOS_1
+//
+//  Created by Victor Danney Garcia Plaza on 12/6/15.
+//  Copyright © 2015 Victor Garcia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
